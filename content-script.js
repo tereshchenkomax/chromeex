2a87f739569475858d295856e37ee6c1c87cf62d
Anton Khorolskiy <antonkhorolskiy@Anton-Khorolskyy-Air.local>
2018-01-26 16:27:53 +0200

    1/26

---
+++ content-script.js
@@ -0,0 +1,14 @@
+(() => {
+
+	// just place a div at top right
+	var div = document.createElement('div');
+	div.style.position = 'fixed';
+	div.style.top = 0;
+	div.style.right = 0;
+	div.textContent = 'Injected!';
+	document.body.appendChild(div);
+	document.getElementsByTagName('div');
+
+	alert('inserted self... giggity');
+
+})();
