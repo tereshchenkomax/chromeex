b7cc439a9187a150e7d9f30f0c6f7234011472b2
Anton Khorolskiy <antonkhorolskiy@Anton-Khorolskyy-Air.local>
2018-02-14 14:16:25 +0200

    The condition which checks the tab URL added

--- content-script.js
+++ content-script.js
@@ -2,17 +2,12 @@
 	var x = [];
 	const hrefs = [];
 
-	// test
-	var a = document.querySelectorAll("a[href^='https://trello.com']");
-	console.log(`${a[0].href} test`);
-
 	//dev
 	x.push(document.querySelectorAll("a[href^='http://do.ready-for-feedback.com']"));
 	x.push(document.querySelectorAll("a[href^='https://trello.com']"));
 	
 	for (var i = x.length - 1; i >= 0; i--) {
-		hrefs.push(x[i][0].href);
-		
+		hrefs.push(x[i][0].href);	
 	}
 	console.log(hrefs);
 	return hrefs;
--- popup.js
+++ popup.js
@@ -2,16 +2,25 @@ document.addEventListener("DOMContentLoaded", () => {
 	var parse = document.getElementById("parse");
 	var trello = document.getElementById("trello");
 	var ready = document.getElementById("ready");
+	var head = document.getElementById("head");
+	console.log(head);
 	var arr = [];
 	parse.addEventListener("click",() => {
-		chrome.tabs.executeScript({
-			file: "content-script.js"
-		},
-		(results) => {
-			arr = results[0];
-			trello.value = arr[0];
-			ready.value = arr[1];
+		chrome.tabs.getSelected(null,function(tab) {
+			var url = tab.url;
+			var testUrl = "https://na43.salesforce.com";
+			if(url.indexOf(testUrl) !== -1) {
+				chrome.tabs.executeScript({
+					file: "content-script.js"
+				}, (results) => {
+					arr = results[0];
+					trello.value = arr[0];
+					ready.value = arr[1];
+				});
+			} else {
+				head.innerHTML = "Open the SF window";
+			}
+			
 		});
-	});
-	
+	});	
 });
