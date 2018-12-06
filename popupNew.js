e640f8936a6b7291bfd5077b2ee97faff25d148e
Anton Khorolskiy <antonkhorolskiy@Max-Tereshchenko-Air.local>
2018-12-06 14:52:42 +0200

    6/12/2018

--- popupNew.js
+++ popupNew.js
@@ -1,32 +1,68 @@
-function varInit() {
-	var parse = $("parse");
-	var sf = $("sf");
-	var copy = $("copy");
-	var paste = $("paste");
-	var trello = $("trello");
-	var ready = $("ready");
-	var password = $("password");
-	var message = $("message");
-	var head = $("head");
-	var value;
-	var taburl;
+class Main {
+	constructor() {
+		this.state = {};
+		this.getTabUrl().bind(this);
+	}
+
+	init() {
+
+		this.state.trello = $("trello");
+		this.state.ready = $("ready");
+		this.state.password = $("password");
+		this.state.message = $("message");
+		this.state.head = $("head");
+		this.checkTheTab();
+
+	}
+
+	checkTheTab() {
+		this.getTabUrl()
+			.then((res) => {
+				let testUrl = /na[0-9]{2}\.salesforce\.com/;
+				if (testUrl.test(res)) {
+					this.state.tabUrl = res;
+					return true;
+				} else {
+					openSF();
+					$("parse").disabled = $("sf").disabled = $("copy").disabled = $("paste").disabled = true;
+					return false;
+				}
+			});
+	}
+
+	getTabUrl() {
+		return new Promise(res => {
+			chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+				let taburl = tabs[0].url;
+				console.log(taburl);
+				res(taburl);
+			});
+		});
+	}
+
+
+	copyAndParse() {
+		$("parse").addEventListener("click", clickHandlerParse);
+		$("copy").addEventListener("click", clickHandlerCopy);
+		$("paste").addEventListener("click", clickHandlerPaste);
+		$("sf").addEventListener("click", clickHandlerSF);
+	}
+
 }
-
-
 
 function injectVar(variable) {
 	chrome.tabs.executeScript({
 		code: `var values = ${JSON.stringify(variable)};`
-	},() => console.log(`${variable} injected`));
+	}, () => console.log(`${variable} injected`));
 }
 
 function injectScript(filename) {
-	return new Promise( res => {	
+	return new Promise(res => {
 		chrome.tabs.executeScript({
 			file: filename
 		}, (results) => {
-			if (results !== undefined && results[0] !== null) {
-				chrome.storage.local.set({"value": results}, function() {
+			if (results != null) {
+				chrome.storage.local.set({"value": results}, function () {
 					console.log(`results: ${results}`);
 				});
 			}
@@ -35,27 +71,10 @@ function injectScript(filename) {
 	});
 }
 
-function checkTheTab() {
-	chrome.tabs.getSelected(null,(tab) => {
-		taburl = tab.url;
-		var testUrl = "https://na43.salesforce.com";
-		if(taburl.indexOf(testUrl) !== -1) {
-			return true;
-		} else {
-			openSF();
-			parse.disabled = true;
-			sf.disabled = true;
-			copy.disabled = true; 
-			paste.disabled = true;
-			return false;
-		}
-	});
-}
-
 function getFromStorage() {
-	return new Promise( res => {
+	return new Promise(res => {
 		chrome.storage.local.get(null, (results) => {
-			if (results.value[0] !== null){
+			if (results.value[0] !== null) {
 				value = results.value[0];
 				res(value);
 			}
@@ -64,7 +83,7 @@ function getFromStorage() {
 }
 
 function clickHandlerCopy() {
-	if (checkTheTab){
+	if (checkTheTab()) {
 		injectScript("copy-script.js");
 	} else {
 		openSF();
@@ -73,20 +92,20 @@ function clickHandlerCopy() {
 
 function clickHandlerPaste() {
 	getFromStorage()
-		.then((value)=>{
+		.then((value) => {
 			injectVar(value);
 			injectScript("paste-script.js");
 		})
-		.catch((Error)=>{
+		.catch((Error) => {
 			console.log(Error);
 		});
 }
 
 function clickHandlerSF() {
 	injectScript("sf-script.js")
-		.then((results)=>{
+		.then((results) => {
 			trello.value = results[0];
-			message.value =`${trello.value} \n ___ \n ${taburl}`;
+			message.value = `${trello.value} \n ___ \n ${taburl}`;
 			message.select();
 			try {
 				var successful = document.execCommand("copy");
@@ -97,28 +116,30 @@ function clickHandlerSF() {
 				console.log(err);
 			}
 		})
-		.catch((Error)=>{
+		.catch((Error) => {
 			console.log(Error);
 		});
-	
+
 }
 
 function clickHandlerParse() {
 	injectScript("content-script.js")
-		.then((results)=>{
+		.then((results) => {
 			ready.value = results[0][0];
 			trello.value = results[0][1];
 			password.value = results[0][2];
 			let text = `Hello {Contact First name},\n
 Your project is completed and ready for review.\n
-In order to review the site, please go to the following link:\n
+In order to review the site, please go to the following links:\n
 ${ready.value}\n
-U: Welcome
-P: ${password.value}\n
+${ready.value}/wp-admin \n
+Username: Welcome
+Password: ${password.value}\n
 Our team uses a simple and effective way to deliver your screenshot feedback directly to us for review using a tool called Userback. It will help you to save much time giving us your feedback. The instructions on getting started are below.\n
-http://do.ready-for-feedback.com/manuals/WLA_Userback_Manual.pdf\n
+https://www.youtube.com/watch?v=4RrGEQhVC6s&feature=youtu.be\n
 Let us know when you are done with reviewing the website and we will get down to work on your feedback.\n
-If you want to follow the process of work on the requested edits you may join this Trello board - ${trello.value}\n
+If you want to follow the process of work on the requested edits you may join this Trello board: - ${trello.value}\n
+The instructions on how it works can be found in the first column.\n
 One important note: due to our policy, the final invoice for the project will be created two weeks after the dev link is delivered. So, please send us your feedback at the earliest convenience.\n
 Looking forward to hearing from you soon!\n
 Best regards,`;
@@ -133,7 +154,7 @@ Best regards,`;
 				console.log(err);
 			}
 		})
-		.catch((Error)=>{
+		.catch((Error) => {
 			console.log(Error);
 		});
 }
@@ -141,19 +162,19 @@ Best regards,`;
 function openSF() {
 	head.innerHTML = "Open the Salesforce tab";
 	head.style.color = "red";
-	setTimeout(() => {head.style.color = "black";}, 2000);
+	setTimeout(() => {
+		head.style.color = "black";
+	}, 2000);
 }
 
 function $(id) {
 	return document.getElementById(id);
 }
 
-function copyAndParse(){
-	checkTheTab();
-	parse.addEventListener("click",clickHandlerParse);
-	copy.addEventListener("click",clickHandlerCopy);
-	paste.addEventListener("click",clickHandlerPaste);
-	sf.addEventListener("click",clickHandlerSF);
-}
 
-document.addEventListener("DOMContentLoaded", copyAndParse);
+document.addEventListener("DOMContentLoaded", () => {
+	let basicclass = new Main();
+	basicclass.init();
+	basicclass.copyAndParse();
+});
+
