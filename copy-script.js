d376cb40579bbaf9324661049f99b2351a953c9f
tereshchenkomax <tereshchenkomax@gmail.com>
2019-02-05 16:10:25 +0200

    class structure is finished

--- copy-script.js
+++ copy-script.js
@@ -4,13 +4,13 @@
 		hrefs.push(document.getElementById("00NF000000Bi4WC_ileinner").children[0].href);
 		hrefs.push(document.getElementById("00NF000000ClNga_ileinner").children[0].href);
 		hrefs.push(document.getElementById("00NF000000D23c6_ileinner").children[0].href);
+		hrefs.push(document.getElementById("00NF000000D23cG_ileinner").children[0].href);
 		hrefs.push(document.getElementById("00NF000000DaF4D_ileinner").outerText);
 		hrefs.push(document.getElementById("00NF000000DaF4I_ileinner").outerText);
-	 	}
-	catch(err) {
+	}
+	catch (err) {
 		console.log(err);
 	}
 
-	console.log(hrefs);
 	return hrefs;
 })();
--- paste-script.js
+++ paste-script.js
@@ -1,17 +1,18 @@
 (() => {
-	
-	console.log(values);
-	var bk = document.getElementById("00NF000000ClNga");
-	var trello = document.getElementById("00NF000000Bi4WC");
-	var dev = document.getElementById("00NF000000D23c6");
-	var name = document.getElementById("00NF000000DaF4D");
-	var pass = document.getElementById("00NF000000DaF4I");
-	var checkbox = document.getElementById("00N0G00000DmoiD");
-
-	bk.value = values[1];
+	// console.log(values);
+	let bk = document.getElementById("00NF000000ClNga");
+	let trello = document.getElementById("00NF000000Bi4WC");
+	let dev = document.getElementById("00NF000000D23c6");
+	let live = document.getElementById("00NF000000D23cG");
+	let name = document.getElementById("00NF000000DaF4D");
+	let pass = document.getElementById("00NF000000DaF4I");
+	let checkbox = document.getElementById("00N0G00000DmoiD");
+	// console.log(bk)
 	trello.value = values[0];
+	bk.value = values[1];
 	dev.value = values[2];
-	name.value = values[3];
-	pass.value = values[4];
+	live.value = values[3]
+	name.value = values[4];
+	pass.value = values[5];
 	checkbox.checked = true;
 })();
