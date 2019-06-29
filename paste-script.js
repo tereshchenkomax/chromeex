99cc2d5798f6bc22e8586fa826be229f2574f561
maxter <tereshchenkomax@gmail.com>
2019-06-29 18:09:24 +0300

    for pull request

--- paste-script.js
+++ paste-script.js
@@ -2,17 +2,15 @@
 	console.log(values);
 	let bk = document.getElementById("00NF000000ClNga");
 	let trello = document.getElementById("00NF000000Bi4WC");
-	let dev = document.getElementById("00NF000000D23c6");
+	let doready = document.getElementById("00NF000000D23c6");
 	let live = document.getElementById("00NF000000D23cG");
-	let name = document.getElementById("00NF000000DaF4D");
-	let pass = document.getElementById("00NF000000DaF4I");
-	let checkbox = document.getElementById("00N0G00000DmoiD");
+	let user = document.getElementById("00NF000000DaF4D");
+	let password = document.getElementById("00NF000000DaF4I");
 	// console.log(bk)
-	trello.value = values[0];
-	bk.value = values[1];
-	dev.value = values[2];
-	live.value = values[3];
-	name.value = values[4];
-	pass.value = values[5];
-	checkbox.checked = true;
+	trello.value = values.trello;
+	bk.value = values.bk;
+	doready.value = values.doready;
+	live.value = values.live;
+	user.value = values.user;
+	password.value = values.password;
 })();
